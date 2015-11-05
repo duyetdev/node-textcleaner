340c2d62852fa62d732c59cb50b8e3eaa74f87fa
Van-Duyet Le <lvduit08@gmail.com>
2015-11-06 03:23:58 +0700

    Mocha test

--- test/test.js
+++ test/test.js
@@ -1,18 +1,16 @@
 var assert = require('assert');
-Ngram = require('../index.js');
+var cleaner = require('../index.js');
 
-describe('Ngrams', function() {
-		it ('should be tokenized', function() {
-			assert.equal(5, "Lê văn Duyệt. Duyệt Developer".tokens().length);
-			assert.equal(3, "Lê văn Duyệt".tokens().length);
-		});
+describe('Node_TextCleaner', function() {
+	it('should be run', function() {
+		console.log(cleaner('le van duyet,,.! so do'))
+	});
 
+	it('should be blank', function() {
+		assert.equal('', cleaner('....!#'));
+	});
 
-		it ('should be ngramed', function() {
-			var ngram = new Ngram();
-			var s = ngram.ngram('le van duyet le van duyet le van duyet');
-
-            assert.equal(9, s.length);
-		});
-
+	it('should be lower after clean', function() {
+		assert.equal('levanduyet', cleaner('LevanDUYET!!!!'));
+	})
 });

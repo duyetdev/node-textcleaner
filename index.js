5c19ee5ec719928a3ad8061f657711085cb9039c
Van-Duyet Le <lvduit08@gmail.com>
2015-11-06 01:52:53 +0700

    Token and ngram

--- index.js
+++ index.js
@@ -56,62 +56,6 @@ Ngrams.prototype.feedAll = function(words) {
     });
 };
 
-/**
- * Sort ngram by popularity
- */
-Ngrams.prototype.ranks = function() {
-    if (this._ranks === undefined) {
-        var g = this;
-        this.keys.sort(function(a, b) {
-            return g.stats[a] - g.stats[b];
-        });
-        var rank = 0;
-        var ranks = {};
-        var before = undefined;
-        this.keys.reverse().forEach(function(key) {
-            if (g.stats[key] != before) {
-                before = g.stats[key];
-                rank += 1;
-            }
-            ranks[key] = rank;
-        });
-        this._ranks = ranks;
-    }
-    return this._ranks;
-};
-
-/**
- * Feed Ngrams from a lm file
- */
-Ngrams.prototype.readLm = function(path) {
-    var lm = fs.readFileSync(path, 'utf8');
-    var n = this;
-    lm.split('\n').forEach(function(line) {
-        var kv = line.split('\t ');
-        //console.log(kv[0].length);
-        if (kv.length > 1 && kv[0].length >= n.min && kv[0].length <= n.max) {
-            n.keys.push(kv[0]);
-            n.stats[kv[0]] = parseInt(kv[1], 10);
-        }
-    });
-};
-
-/**
- * Distance between two Ngrams
- */
-Ngrams.prototype.distance = function(other) {
-    var distance = 0;
-    var n = this;
-    this.keys.forEach(function(key) {
-        if (other.stats[key] === undefined) {
-            distance += 2000;
-        } else {
-            distance += Math.abs(n.ranks()[key] - other.ranks()[key]);
-        }
-    });
-    return distance;
-};
-
 exports.Ngrams = Ngrams;
 
 String.prototype.tokens = function(filter) {
@@ -120,6 +64,6 @@ String.prototype.tokens = function(filter) {
         if (filter !== undefined) {
             return filter.call(s, word);
         }
-        return word.toLowerCase();
+        return cleanup(word).toLowerCase();
     });
 };

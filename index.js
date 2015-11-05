833cb4f07ed34c435d8c793b5683c3ff1153d437
Van-Duyet Le <lvduit08@gmail.com>
2015-11-06 02:25:48 +0700

    ngram using lodash, remove some bug

--- index.js
+++ index.js
@@ -1,118 +1,46 @@
 var util = require('util');
 var fs = require('fs');
+var _ = require('lodash');
+
 
 function cleanup(text) {
     return text.replace(/(^[\\("']+)|([,:;.?!)"'|\\]+$)/, '').toLowerCase();
 }
 
-function ngram(word, min, max) {
-    var ngrams = [];
-    word = '_' + word + '_';
-    for (var i = 0; i < word.length; i++) {
-        for (var s = min; s <= max; s++) {
-            if (i + s <= word.length) {
-                var ngram = word.substr(i, s);
-                if (ngram != '_')
-                    ngrams.push(ngram);
-            }
-        }
-    }
+var Ngrams = function(options) {
+    var options = options || {};
 
-    return ngrams;
-}
+    this.n = options.n || 2;
 
-exports.ngram = ngram;
-
-var Ngrams = function() {
     this.keys = [];
-    this.stats = {};
-    this.min = 1;
-    this.max = 4;
-    this._ranks = undefined;
-};
-
-Ngrams.prototype.feed = function(word) {
-    this._ranks = undefined;
-    var g = this;
-    ngram(word, this.min, this.max).forEach(function(n) {
-        if (g.stats[n] === undefined) {
-            g.stats[n] = 1;
-            g.keys.push(n);
-        } else {
-            g.stats[n] += 1;
-        }
-    });
 };
 
 /**
  * build ngrams from a sentences, an array of words
  */
-Ngrams.prototype.feedAll = function(words) {
-    if (typeof words === 'string')
-        words = words.tokens();
+Ngrams.prototype.ngram = function(sentences, n) {
+    if (typeof sentences === 'string')
+        words = sentences.tokens();
+    else if (typeof sentences === 'array')
+        words = sentences; // Is array
+    else 
+        throw new Error("Input must be String or Array");
+
     var g = this;
-    words.forEach(function(word) {
-        g.feed(word);
-    });
-};
 
-/**
- * Sort ngram by popularity
- */
-Ngrams.prototype.ranks = function() {
-    if (this._ranks === undefined) {
-        var g = this;
-        this.keys.sort(function(a, b) {
-            return g.stats[a] - g.stats[b];
-        });
-        var rank = 0;
-        var ranks = {};
-        var before = undefined;
-        this.keys.reverse().forEach(function(key) {
-            if (g.stats[key] != before) {
-                before = g.stats[key];
-                rank += 1;
-            }
-            ranks[key] = rank;
-        });
-        this._ranks = ranks;
+    var ngramNumber = n || this.ngram || 2;
+    var ngrams = [];
+    var l = words.length;
+    
+    for (var i = 0; i < l; i++) {
+        var ngram = _.slice(words, i, _.min(i+ngramNumber, l));
+        ngrams.push(ngram);
     }
-    return this._ranks;
+
+    return ngrams;
 };
 
-/**
- * Feed Ngrams from a lm file
- */
-Ngrams.prototype.readLm = function(path) {
-    var lm = fs.readFileSync(path, 'utf8');
-    var n = this;
-    lm.split('\n').forEach(function(line) {
-        var kv = line.split('\t ');
-        //console.log(kv[0].length);
-        if (kv.length > 1 && kv[0].length >= n.min && kv[0].length <= n.max) {
-            n.keys.push(kv[0]);
-            n.stats[kv[0]] = parseInt(kv[1], 10);
-        }
-    });
-};
-
-/**
- * Distance between two Ngrams
- */
-Ngrams.prototype.distance = function(other) {
-    var distance = 0;
-    var n = this;
-    this.keys.forEach(function(key) {
-        if (other.stats[key] === undefined) {
-            distance += 2000;
-        } else {
-            distance += Math.abs(n.ranks()[key] - other.ranks()[key]);
-        }
-    });
-    return distance;
-};
-
-exports.Ngrams = Ngrams;
+module.exports = Ngrams;
 
 String.prototype.tokens = function(filter) {
     var s = this;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var assert = require('assert');
-ngram = require('..');
+Ngram = require('../index.js');
+
+console.log(Ngram + '')
 
 describe('Ngrams', function() {
 		it ('should be tokenized', function() {
@@ -8,13 +10,14 @@ describe('Ngrams', function() {
 			assert.equal(3, "Lê văn Duyệt".tokens().length);
 		});
 
-		/*
+
 		it ('should be ngramed', function() {
-			var n = new ngram.Ngrams();
-			n.min = 3;
-			n.feedAll('le van duyet le van duyet le van duyet');
-			var ranks = n.ranks();
-            assert.equal(63, n.keys.length);
+			var ngram = new Ngram();
+			var s = ngram.ngram('le van duyet le van duyet le van duyet');
+
+			console.log(s)
+
+            assert.equal(63, s.length);
 		});
-*/
+
 });

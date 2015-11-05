2b71c7a5d94d3167c3f16c15c99dd71b9b9403f7
Van-Duyet Le <lvduit08@gmail.com>
2015-11-06 02:30:29 +0700

    Update unit test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,8 @@
 var assert = require('assert');
 Ngram = require('../index.js');
 
-console.log(Ngram + '')
-
 describe('Ngrams', function() {
 		it ('should be tokenized', function() {
-			console.log("Lê văn Duyệt. Duyệt Developer".tokens())
 			assert.equal(5, "Lê văn Duyệt. Duyệt Developer".tokens().length);
 			assert.equal(3, "Lê văn Duyệt".tokens().length);
 		});
@@ -15,9 +12,7 @@ describe('Ngrams', function() {
 			var ngram = new Ngram();
 			var s = ngram.ngram('le van duyet le van duyet le van duyet');
 
-			console.log(s)
-
-            assert.equal(63, s.length);
+            assert.equal(9, s.length);
 		});
 
 });
